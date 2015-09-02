a73dc2c65c8947ebecaf5ca3df24258202b6f399
alexnicolini <alexnicolini@gmail.com>
2015-09-02 20:49:46 -0300

    Count the total number of tasks and the number of completed tasks from the currently selected list

--- client/views/todos_count.js
+++ client/views/todos_count.js
@@ -1,9 +1,15 @@
 Template.todosCount.helpers({
   'totalTodos': function () {
-    return Todos.find().count();
+
+    var currentList = this._id;
+
+    return Todos.find({ listId: currentList }).count();
   },
 
   'completedTodos': function() {
-    return Todos.find({ completed: true }).count();
+
+    var currentList = this._id;
+
+    return Todos.find({ listId: currentList, completed: true }).count();
   }
 });
