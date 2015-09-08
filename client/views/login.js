8a676487007c42c152842eae7f8b2509c7310072
alexnicolini <alexnicolini@gmail.com>
2015-09-08 17:43:54 -0300

    Defining template hooks and custom rules

--- client/views/login.js
+++ client/views/login.js
@@ -3,20 +3,54 @@ Template.login.events({
     
     event.preventDefault();
 
-    var email = $('[name=email]').val();
-    var password = $('[name=password').val();
+    // var email = $('[name=email]').val();
+    // var password = $('[name=password').val();
 
-    // Use account-password methods to log in the user
-    Meteor.loginWithPassword(email, password, function(error) {
-      if (error) {
-        console.log(error.reason);
-      } else {
-        var currentRoute = Router.current().route.getName();
+    // // Use account-password methods to log in the user
+    // Meteor.loginWithPassword(email, password, function(error) {
+    //   if (error) {
+    //     console.log(error.reason);
+    //   } else {
+    //     var currentRoute = Router.current().route.getName();
 
-        if (currentRoute == 'login') {
-          Router.go('home');
-        }
-      }
-    });
+    //     if (currentRoute == 'login') {
+    //       Router.go('home');
+    //     }
+    //   }
+    // });
   }
+});
+
+Template.login.onCreated(function() {
+  // console.log('The "login" template was just created.');
+});
+
+Template.login.onRendered(function() {
+  $('.login').validate({
+    rules: {
+      email: {
+        // required: true,
+        email: true
+      },
+      password: {
+        // required: true,
+        minlength: 6
+      }
+    },
+    messages: {
+      email: {
+        required: 'You must enter an email address.',
+        email: "You've entered an invalid email address."
+      },
+      password: {
+        required: 'You must enter a password.',
+        minlength: 'Your password must be at least {0} characters.'
+      }
+    }
+  });
+  // console.log('The "login" template was just rendered.');
+});
+
+Template.login.onDestroyed(function() {
+  // console.log('The "login" template was just destroyed.');
 });
--- client/views/register.js
+++ client/views/register.js
@@ -3,23 +3,49 @@ Template.register.events({
     
     event.preventDefault();
 
-    var email = $('[name=email]').val();
-    var password = $('[name=password]').val();
+    // var email = $('[name=email]').val();
+    // var password = $('[name=password]').val();
 
-    // Use account-password methods to register the user
-    Accounts.createUser({
-      email: email,
-      password: password
-    }, function(error) {
-          if (error) {
-            console.log(error.reason);
-          } else {
-            // Redirect user if registration succeeds
-            Router.go('home'); 
-          }                   
-    });
+    // // Use account-password methods to register the user
+    // Accounts.createUser({
+    //   email: email,
+    //   password: password
+    // }, function(error) {
+    //       if (error) {
+    //         console.log(error.reason);
+    //       } else {
+    //         // Redirect user if registration succeeds
+    //         Router.go('home'); 
+    //       }                   
+    // });
 
-    // Redirect to Home page
-    Router.go('home');
+    // // Redirect to Home page
+    // Router.go('home');
   }
+});
+
+Template.register.onRendered(function() {
+  $('.register').validate({
+    rules: {
+      email: {
+        // required: true,
+        email: true
+      },
+      password: {
+        // required: true,
+        minlength: 6
+      }
+    },
+    messages: {
+      email: {
+        required: 'You must enter an email address.',
+        email: "You've entered an invalid email address."
+      },
+      password: {
+        required: 'You must enter a password.',
+        minlength: 'Your password must be at least {0} characters.'
+      }
+    }
+
+  });
 });
