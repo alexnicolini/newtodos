461dda04de98cf6777006b3c07da81d68c56b175
alexnicolini <alexnicolini@gmail.com>
2015-09-09 20:35:39 -0300

    Added Template subscriptions

--- client/views/lists.js
+++ client/views/lists.js
@@ -1,6 +1,14 @@
 // "Catch" the selection of data from the Meteor.publish function
 // Meteor.subscribe('lists');
 
+// The code in this function will execute as soon as 
+// the template is rendered in the interface (meaning, 
+// when the template is inserted into the DOM)
+Template.lists.onCreated(function() {
+  // Template-level subscription
+  this.subscribe('lists');
+});
+
 Template.lists.helpers({
   'list': function () {
 
